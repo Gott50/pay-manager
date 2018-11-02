b81d6309680746937835c20d156a02ea98e4b066
Gott50 <xxGott50xx@gmail.com>
2018-11-02 11:29:47 +0100

    add RoutePrefix /pay

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
+@app.route('/pay/')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/client_token/', methods=['GET'])
+@app.route('/pay/client_token/', methods=['GET'])
 def new_checkout():
     # app.logger.info('new_checkout(%s)' % customer_id)
     return str(gateway.generate_client_token())
