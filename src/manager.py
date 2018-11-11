a5ef2ce1905886884b2c65cead61e25504fe6473
Gott50 <xxGott50xx@gmail.com>
2018-11-11 14:11:50 +0100

    fix ModuleNotFoundError: No module named 'psycopg2'

--- src/manager.py
+++ src/manager.py
@@ -13,11 +13,20 @@ class Manager:
         self.models = models
 
     def manage(self):
-        result = gateway.get_subscriptions()
-        print(result)
-        for subscription in result.items:
-            print("%s: status: %s; email: %s" %
-                  (subscription.id, subscription.status, subscription.transactions[-1].customer['email']))
+        for subscription in gateway.get_subscriptions().items:
+            self.update_user(subscription)
+
+    def update_user(self, subscription):
+        email = subscription.transactions[-1].customer['email']
+        print("%s: status: %s; email: %s" %
+              (subscription.id, subscription.status, email))
+        user = self.models.User.query.filter_by(email=email).first()
+        print("set: %s" % subscription.status == 'Active')
+        if user:
+            user.paid = subscription.status == 'Active'
+        else:
+            print("User with email not found: %s" % email)
+        self.db.session.commit()
 
     def run(self):
         while not sleep(10):
