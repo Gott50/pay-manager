e59aaf135e11db71b51c0766d385327f74529dcd
Gott50 <xxGott50xx@gmail.com>
2019-05-19 23:04:39 +0200

    cleanup: remove unused Routes

--- src/app.py
+++ src/app.py
@@ -10,18 +10,6 @@ app = Flask(__name__)
 
 CORS(app)
 
-
-@app.route('/pay/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/pay/client_token/', methods=['GET'])
-def new_checkout():
-    # app.logger.info('new_checkout(%s)' % customer_id)
-    return str(gateway.generate_client_token())
-
-
 @app.route('/pay/purchase/', methods=['POST'])
 def create_subscription():
     data = json.loads(request.data)
